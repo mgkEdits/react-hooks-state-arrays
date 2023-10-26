7ba3d21f81980bae70e7231bfcb2b4e5bd192397
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-10-26 23:50:49 +0300

    completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -10,17 +10,45 @@ function SpicyFoodList() {
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
+  const [filterBy, setFilterBy] = useState("All");
+
+  function handleFilterChange(event) {
+  setFilterBy(event.target.value);
+}
+
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
+      <select name="filter" onChange={handleFilterChange}>
+    <option value="All">All</option>
+    <option value="American">American</option>
+    <option value="Sichuan">Sichuan</option>
+    <option value="Thai">Thai</option>
+    <option value="Mexican">Mexican</option>
+  </select>
       <ul>{foodList}</ul>
     </div>
   );
 }
+
 
 export default SpicyFoodList;
